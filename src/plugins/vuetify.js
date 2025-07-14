338f476aeb36e25317177c0c6deb3fc3cb9b8896
Sonny A. Nepomuceno <son@hrd-s.com>
2025-07-14 11:12:50 +0800

    initial

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -3,7 +3,8 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
-import '@mdi/font/css/materialdesignicons.css'
+import '@mdi/font/css/materialdesignicons.css';
+
 
 const vuetify = createVuetify({
     components,
